aa809bbfeb61eff356eb3c9080c6ab7cc837486b
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2013-11-28 11:14:49 +0700

    config database to ipat.in.th

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'host'      => 'ipat.in.th',
+			'database'  => 'ipatinth_pcu',
+			'username'  => 'ipatinth_pcu',
+			'password'  => 'Cheer1234',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
