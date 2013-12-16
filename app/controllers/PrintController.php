f0b8444e25660f399624896772a83f77ccba593c
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2013-12-16 17:18:19 +0700

    i forgot to turn the validation on.

--- app/controllers/PrintController.php
+++ app/controllers/PrintController.php
@@ -25,31 +25,7 @@ class PrintController extends BaseController {
 
 	//This function generate the PDF file.
 	public function getPdf(){
-		$htmlContent = View::make('print.test');
-		 // convert in PDF
-		//$path = app_path() . '/libs/html2pdf.class.php';
-		$path = app_path() . 'fuckyououuuuuu';
-
-		$path = 'aoeu';
-
-		//return var_dump(file_exists($path));
-		echo $path . '<br>';
-		echo var_dump(file_exists($path));
-		require_once($path);
-		//return 'aoeuaoeu';
-// 		try
-// 		{
-// 			$pdf = new HTML2PDF('P', 'A4', 'fr');
-// //      $html2pdf->setModeDebug();
-// 			$pdf->setDefaultFont('Arial');
-// 			$pdf->writeHTML($content, isset($_GET['vuehtml']));
-// 			$pdf->Output('exemple00.pdf');
-// 			return 'finish';
-// 		}
-// 		catch(HTML2PDF_exception $e) {
-// 			return $e;
-// 		}
-		//return $htmlContent;
+		//Write PDF Generating code here!
 	}
 
 	//just for debugging
