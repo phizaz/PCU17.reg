888e849bbdd3c39ba0c040548c9c2bd9b52dfd08
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2013-12-29 12:06:31 +0700

    change link at views/print.php

--- app/views/print.php
+++ app/views/print.php
@@ -4,6 +4,6 @@
 	</head>
 	<body>
 		ยินดีต้อนรับ คุณ<?=$credential->name_first . ' ' . $credential->name_last?>
-		<a href="print/fpdf">Print File</a>
+		<a href="print/pdf">Print File</a>
 	</body>
 </html>
