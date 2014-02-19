c19f4514a1b057b7ef15c8f8450b25850ccc00a1
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2014-02-19 20:24:04 +0700

    the print page. finishing up.

--- app/views/print.php
+++ app/views/print.php
@@ -37,63 +37,82 @@
 		</div>
 		
 		<div class="prefix_12 grid_12">
-			<div style="height: 60px;"></div>			
+			<div style="padding-left: 20px;">
+				<div style="height: 60px;"></div>			
 				<?php 
-					if(!$errors->first()==""){
-						echo "<div class=\"error\">";
-						foreach ($errors->all() as $message)
-						{
-						    echo $message . "<br>";
-						}
-						echo "</div>";
+				if(!$errors->first()==""){
+					echo "<div class=\"error\">";
+					foreach ($errors->all() as $message)
+					{
+						echo $message . "<br>";
 					}
+					echo "</div>";
+				}
 				?>
-			<div>
-				<p>
-					<h1>พิมพ์ใบสมัคร</h1>
-					<hr>
-					<h2>คุณ <?php echo $credential->name_first . ' ' . $credential->name_last; ?></h2>
-				</p>
-			</div>
-			
-			<div class="big-button">		
-				<a href="print/pdf">			
-				<img src="<?php echo $url . '/img/pdf.png' ?>">
-				<br>
-				<div>พิมพ์ใบสมัคร</div>
-				</a>
-			</div>
+				<div>
+					<p>
+						<h1 class="grey">พิมพ์ใบสมัคร <small>คุณ<?php echo $credential->name_first . ' ' . $credential->name_last; ?></small></small></h1>
+					</p>
+				</div>
 
-			<div class="big-button">		
-				<a id="link-detail" href="#">			
-				<img src="<?php echo $url . '/img/next.png' ?>">
-				<br>
-				<div>ขั้นตอนต่อไป</div>
-				</a>
-			</div>
-			<div style="height:100px"></div>
-			<div style="clear:both"></div>
-			<hr style="height:10px;border:none;color:#333;background-color:#333;">
-			<a name="details"/>
-			<div>
-				<p>
-					<h1>รายละเอียดต่อไป</h1>
-					<hr>
-					<h2>ข้าวมันไก่โก๊ะตี๋ อร่อยที่สุดในโลกเหรอครับ </h2>
-					<p>ครึ้มอกครึ้มใจ วันนี้วันหยุด ที่ทำงานก็หยุด และไม่มีแผปนไปต่างจังหวัด คนกรุงเทพฯอย่างเราเลยตั้งใจขับรถไปดูหนังที่เมเจอร์รังสิต กะว่าจะไปดูไอ้เรื่องที่มันส์ๆ ฮาๆ กวาดรายได้ไปหลายร้อยล้านบาท แต่กลับพบว่าคนที่คิดเหมือนเรามากมายเหลือเกิน ตัดสินใจอยู่พักนึง จนสุดท้ายก็ไม่ดู ไปหาข้าวไปทานที่บ้านดีกว่า</p>
-					<p>เดินลงมาชั้นล่าง เเห็นข้าวมันไก่โก๊ะตี๋ อืมม์ น่าลองเหมือนกัน เลยสั่งกลับบ้าน1ที่ หลังจากรออยู่พักนึง พนักงานก็เรียกเก็บเงินครับ</p>
-					<p>"91บาทค่ะ" สั้นๆ แต่ทำใจผมสั่นได้ ด้วยความใจดีสู้เสือ ถามไปอีกครั้งนึงก็ได้คำตอบมาเหมือนเดิม แถมกดเครื่องคิดเลขพร้อมอธิบายให้ฟังด้วยว่า ค่าข้าวน่ะ 75 บาท ค่ากล่องอีก 10บาท และพอกด VAT ก็จะได้ 91 บาทครบถ้วนเป๊ะๆ</p>
-					<p>ด้วยความมึนงงเหมือนโดนทุบด้วยค้อน 1,000 ปอนด์ ผมเดินกลับมาที่รถด้วยความรู้สึกสับสน พลางคิดในใจ ว่า แม่ม!!! คงเอาไก่ฟ้าพระยาลอมาต้มให้กินเป็นแน่แท้ น้ำจิ้มคงมีส่วนผสมของบัวพันปีที่หาได้จากยอดเขาหิมาลัย ในขณะที่น้ำซุปคงทำจากน้ำอายุวรรธณะ ทำให้อยู่ยงคงกระพันตลอดไป</p>
-					<h3>มีเรื่องกะโก๊ะตี๋ส่งมาที่</h3>
-					<div class="add-box">
-						100 ถนน สิบแปดเมตร <br>
-						แขวง บางรัก เขต บางรัก <br>
-						กรุงเทพมหานคร <br>
-						10500 <br>
+				<a href="print/pdf" target="_blank">
+					<div class="big-button">
+						<div class="img">
+							<h4 class="btn btn-success">พิมพ์ใบสมัคร</h4>
+						</div>
 					</div>
+				</a>
+				<p style="color: rgb(88,88,88);">
+					หลังจากพิมพ์ใบสมัครแล้ว<br>
+					กรุณาเซ็นต์ชื่อในตำแหน่งที่กำหนด <br>
+					และตอบคำถาม ข้อเขียนในส่วนสุดท้าย<br>
+					<strong>อย่าลืมติดรูปนักเรียนในใบสมัครด้วย</strong>
 				</p>
+
+				<div style="clear:both"></div>
+				<div style="height:30px"></div>
+				
+				<a name="details"/>
+				<div style="color: rgb(88,88,88);">
+					<h1 class="grey">ส่งใบสมัคร</h1>
+					<p>
+						ส่ง <strong>ใบสมัคร</strong> (ที่ติดรูปแล้ว) พร้อมรูปถ่ายขนาด 3 x 4 ซม. <strong>อีกหนึ่งรูป</strong> มาที่
+					</p>
+					<div class="group group-pink">
+						aoeu
+					</div>
+
+					<p>
+						รอการประกาศผลการทัดเลือก (วันที่ ...) เข้าค่ายที่เว็บไซต์นี้ หรือที่เพจ Facebook
+					</p>
+					<div class="group group-pink">
+						aoeu
+					</div>
+					
+					<div style="height: 15px;"></div>
+					<p>
+						เมื่อทราบว่า <strong>ได้รับการคัดเลือก</strong> โปรดโอนเงิน <strong>จำนวน 3,000 บาท</strong> มาที่
+					</p>
+					<div class="group group-pink">
+						aoeu
+					</div>
+
+					<p>
+						เก็บหลักฐานการโอินเงิน (สลิปการโอน) และส่งมาพร้อมกับ
+					</p>
+					<ul>
+						<li>สำเนาบัตรประจำตัวประชาชน</li>
+						<li>สำเนาทะเบียนบ้าน</li>
+					</ul>
+					<p>มาที่</p>
+					<div class="group group-pink">
+						aoeu
+					</div>
+
+
+				</div>
+
 			</div>
-			
 		</div>
 	</div>
 	<div style="height: 25px;"></div>
